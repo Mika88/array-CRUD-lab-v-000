3328719e1ba8b87c711393e8d927e151c1b3416e
Mika88 <mikaspivak@gmail.com>
2018-07-19 03:22:20 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
- Array = [love, life, peace, flowers]
+ array = [love, life, peace, flowers]
 end
 
 def add_element_to_end_of_array(array, element)
